b6372a94e5e9972dd9223ac1110d7c45ce503781
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-12 00:38:51 +0530

    05_nonPrimitive.js

---
+++ src/js/05_nonPrimitive.js
@@ -0,0 +1,17 @@
+// Datatype Object: Represents key-value pairs
+let obj = {
+    name: "Nilesh",
+    age: 23
+};
+console.log(obj);
+
+// Datatype Array: Represents an ordered list of values
+let a = ["A", "B", "C"];
+console.log(a);
+
+// Datatype Function: Represents reusable blocks of code
+function myFunc() {
+    console.log("JavaScript Mastering");
+}
+console.log(myFunc);
+myFunc();
