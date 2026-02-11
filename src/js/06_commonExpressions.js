207722d624d15b9c6adff352aab11f35073be44b
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-12 00:55:50 +0530

    Add 06_commonExpressions.js

---
+++ src/js/06_commonExpressions.js
@@ -0,0 +1,10 @@
+console.log(null === undefined);
+console.log(5 > 3 > 2);
+console.log([1,2] === [1,2]);
+console.log("10" < "9");
+console.log(NaN === NaN);
+console.log(true == 1);
+console.log(undefined > 0);
+console.log("5" === 5)
+console.log([1, 2] == [1, 2]);
+console.log(Infinity > 1000)
