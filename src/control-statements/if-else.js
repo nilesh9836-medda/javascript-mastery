8e403624bce8b25dd3581d6a98b560cff9872384
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-18 22:52:37 +0530

    Add if-else statement

---
+++ src/control-statements/if-else.js
@@ -0,0 +1,21 @@
+// if statement
+var a = 5
+if(a>0){
+    console.log("Positive");
+};
+
+// if else statement
+if (a%2==0)
+    console.log("Even");
+else
+    console.log("Odd");
+
+// else if statement
+if (a>0)
+    console.log("Positive");
+else if (a === 0)
+    console.log("Zero");
+else if (a<0)
+    console.log("Negative");
+else
+    console.log(NaN);
