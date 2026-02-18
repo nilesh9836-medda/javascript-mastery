4b8dd46b9b84c9a32ce0e313c68c79a00140b8a9
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-19 03:48:49 +0530

    Add switch case

---
+++ src/control-statements/switch-case.js
@@ -0,0 +1,15 @@
+var a = 5;
+
+switch(a){
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One")
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    default:
+        console.log("Greater than two")
+}
