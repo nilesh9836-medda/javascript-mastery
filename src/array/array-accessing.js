5b2879bd78c30a0131472a379636c7bfab951af2
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-20 20:50:18 +0530

    Add array elements accessing

---
+++ src/array/array-accessing.js
@@ -0,0 +1,9 @@
+a = [1,2,3,4,5];
+console.log(a[0])
+console.log(a[1])
+
+// Accessing first item of an array
+console.log(`First: ${a[0]}`);
+
+// Accessing last item of an array
+console.log("Last: ",a[a.length - 1]);
