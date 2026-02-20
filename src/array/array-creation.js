d1f5b0f5b76a9e3f696ae81867d61251ca1b32da
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-20 20:29:02 +0530

    Add array creation

---
+++ src/array/array-creation.js
@@ -0,0 +1,15 @@
+// 1. Create Array using Literal
+
+// Creating an Empty Array
+let a = [];
+console.log(a);
+
+// Creating an Array and Initializing with Values
+let b = [10, 20, 30];
+console.log(b);
+
+// 2. Create using new Keyword (Constructor)
+
+// Creating and Initializing an array with values
+let a1 = new Array(10, 20, 30);
+console.log(a1);
