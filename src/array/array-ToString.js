6c74bdee345f2beab288bac45b7bfd9061e2c9c4
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-20 21:27:10 +0530

    Add array to string and array concate

---
+++ src/array/array-ToString.js
@@ -0,0 +1,13 @@
+// Creating an Array and Initializing with Values
+let a = ["HTML", "CSS", "JS"];
+
+// Convert array ot String
+console.log(a.toString());
+
+// Creating an Array and Initializing with Values
+let b = ["Node.js", "Expess.js"];
+
+// Concatenate both arrays
+let concateArray = a.concat(b);
+
+console.log("Concatenated Array: ", concateArray);
