8bf4dbc7c65e21467bd4ddd08fcb6b9936717780
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-20 21:24:19 +0530

    Add array iterating

---
+++ src/array/array-iterating.js
@@ -0,0 +1,12 @@
+// Creating an Array and Initializing with Values
+let a = [1,2,3,4,5,6];
+
+// Iterating through for loop
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i])
+}
+
+// Iterating through forEach loop
+a.forEach(function myfunc(x) {
+    console.log(x);
+});
